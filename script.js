82278d64c43c0ceac97eebaf0f8c067ea171356c
Rodrigo Ortolani <110072051+Ortolani077@users.noreply.github.com>
2024-02-29 21:17:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,32 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const collapseOurServices = document.getElementById("collapseOurServices");
 
   buttonSkills.addEventListener("click", function () {
-    collapseWhoWeAre.classList.toggle("show");
-    collapseOurServices.classList.remove("show");
-  });
-
-  buttonAboutMe.addEventListener("click", function () {
-    collapseOurServices.classList.toggle("show");
-    collapseWhoWeAre.classList.remove("show");
-  });
-
-  document.addEventListener("click", function (event) {
-    const targetCollapse = event.target.closest('.custom-collapse');
-    const isOutsideCollapse = !targetCollapse && !event.target.classList.contains('glow-on-hover') && !event.target.classList.contains('glow-on-hover2');
-
-    if (isOutsideCollapse) {
-      collapseWhoWeAre.classList.remove("show");
-      collapseOurServices.classList.remove("show");
-    }
-  });
-});
-document.addEventListener("DOMContentLoaded", function () {
-  const buttonSkills = document.querySelector(".glow-on-hover");
-  const buttonAboutMe = document.querySelector(".glow-on-hover2");
-  const collapseWhoWeAre = document.getElementById("collapseWhoWeAre");
-  const collapseOurServices = document.getElementById("collapseOurServices");
-
-  buttonSkills.addEventListener("click", function () {
       collapseWhoWeAre.classList.toggle("show");
       collapseOurServices.classList.remove("show");
   });
